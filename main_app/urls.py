9139b03a607b0499442617f327acc0877f6114fe
ksprin <kayla.sprincis@gmail.com>
2023-11-26 13:57:02 -0500

    progress on web

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name = 'home'),
+    path("aboutme", views.skills, name= 'skills'),
+    path("whyigem", views.right, name = 'right'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 
 def home(request):
     return render(request, "main_app/home.html")
+
+def skills(request):
+    return render(request, "main_app/skills.html")
+
+def right(request):
+    return render(request, "main_app/right.html")
+    
